951a34a553c04b7f4acc4a99fec66a750c221709
imba-tjd <109224573@qq.com>
2023-02-16 11:13:41 +0800

    update

--- Readme.md
+++ Readme.md
@@ -1,5 +1,9 @@
 # MTool Manual Trans Script
 
-Accroding to the author, it's allowed to translate and share ManualTransFile.json freely given that not been processed by mtool itself.
+Accroding to the author, it's allowed to translate and share *ManualTransFile.json* freely given that not been processed by mtool itself.
 
 This script doesn't come with translator api in it. You must offer a function for `process` or `process_file`.
+
+This script changes *ManualTransFile.json*. If key != value, it's considered as traslated.
+
+Later, rename *ManualTransFile.json* to *TrsData.json*.
--- mmts.py
+++ mmts.py
@@ -18,22 +18,41 @@ def process(data: dict[str, str], translator: TranslateService):
         try:
             translated = translator(value)
         except Exception:
-            print("Failed to translate %s" % value)
+            print('Failed to translate %s' % value)
             raise
 
         if EnablePrint:
             count += 1
-            print("\rTranslated %d/%d %d%%" % (count, total, count/total*100), end="")
+            print('\rTranslated %d/%d %d%%' % (count, total, count/total*100), end='')
 
         data[key] = translated
 
 
-def process_file(translator: TranslateService, filename="ManualTransFile.json"):
+def process_file(translator: TranslateService, filename='ManualTransFile.json'):
     with open(filename, encoding='u8') as f:
         data = json.load(f)
 
     try:
         process(data, translator)
     finally:
-        with open(filename, "w", encoding='u8') as f:
+        with open(filename, 'w', encoding='u8') as f:
             json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+def data2lines(data: dict[str, str]):
+    for key in data:
+        yield key
+
+
+def lines2data(lines: typing.Iterable[str], data: dict[str, str]):
+    for key, value in zip(data, lines):
+        data[key] = value
+
+
+def file2lines(filename='ManualTransFile.json'):
+    with open(filename, encoding='u8') as f:
+        data = json.load(f)
+
+    with open('ManualTransLines.txt', 'w', encoding='u8') as f:
+        for line in data2lines(data):
+            f.write(line + '\n')
